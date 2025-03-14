16467c7341a2109be188e8c1f7b58734edd00c64
andresfelipetaimallopez <felipeandres0413@gmail.com>
2025-03-14 19:35:15 +0100

    introducion a python

---
+++ uso_de_python.py
@@ -0,0 +1,33 @@
+#autor:andres felipe taimal
+#este es un comentario de una sola linea
+
+
+
+print("estudiado python")
+#defiicion de variables
+#python es sensible a minusculas y mayusculas
+#no se puede usar espacios
+#no se pueden utilizar palbras reservadas
+#no es conveniente usar numeros como nombres de varables
+x=50
+y=15
+
+print(x+y)
+
+print(type(x))
+#<class int> es un numero x entero
+nombre="andres"
+print(nombre)
+print(type(nombre))
+#<clas str> : significa que la variable nombre es un string (cadena)
+
+print(type(x))
+x=4,8
+print(type(x))
+#<class float>: significa que lavariable es un flotante (numero decimal)
+
+print(x*nombre)
+
+
+
+
